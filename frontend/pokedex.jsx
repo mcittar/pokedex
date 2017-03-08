9ad4cec2d87f9eb7b1a2df52e1cd548373e4a5d5
App Academy Student <student@appacademy.io>
2017-03-08 12:06:13 -0800

    Add actions reducers and store

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const rootEl = document.getElementById('root');
+  const store = configureStore();
+  window.store = store;
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
